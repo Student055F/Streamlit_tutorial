fa3621511a22d39d8dcc4532ff5a9f366e55da13
OF-INFS772 <161268769+Student055F@users.noreply.github.com>
2024-03-26 18:25:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ np.object = object
 
 @st.cache(suppress_st_warning=True)
 def get_fvalue(val):
-    feature_dict = {"No":1,"Yes":2}
+    feature_dict = {"No":1, "Yes":2}
     for key,value in feature_dict.items():
         if val == key:
             return value
 
-def get_value(val,my_dict):
+def get_value(val, my_dict):
     for key,value in my_dict.items():
         if val == key:
             return value
